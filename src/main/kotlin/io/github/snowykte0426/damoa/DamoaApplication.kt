31df3cc93c1e93c8daf935b9c91a80fe9b04fb64
snowykte0426 <snowykte0426@naver.com>
2026-06-11 16:09:41 +0900

    init(global): Gradle로 프로젝트 구조를 초기화하고 애플리케이션 구성을 추가하고 기본 파일을 설정

---
+++ src/main/kotlin/io/github/snowykte0426/damoa/DamoaApplication.kt
@@ -0,0 +1,11 @@
+package io.github.snowykte0426.damoa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DamoaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DamoaApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/snowykte0426/damoa/DamoaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.snowykte0426.damoa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DamoaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
